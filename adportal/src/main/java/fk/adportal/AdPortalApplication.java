54b0942f6100dfad329cbf30fb5d66f2a35d6593
Filip Kaszuba <fkaszuba93@gmail.com>
2023-01-16 15:15:30 +0100

    create spring project

---
+++ adportal/src/main/java/fk/adportal/AdPortalApplication.java
@@ -0,0 +1,13 @@
+package fk.adportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdPortalApplication.class, args);
+	}
+
+}
---
+++ adportal/src/test/java/fk/adportal/AdPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package fk.adportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
