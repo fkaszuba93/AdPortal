819cbee91cc50454e4e3d514aafde4986470ed1c
Filip Kaszuba <fkaszuba93@gmail.com>
2023-01-31 15:34:11 +0100

    hide Add offer link on edit page

--- ad-portal/src/components/Header.js
+++ ad-portal/src/components/Header.js
@@ -1,14 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation();
+
+    const isEditPage = () => {
+        return location.pathname.startsWith('/edit');
+    };
+    
     return (
         <>
             <nav className="navbar navbar-expand-sm navbar-light bg-light fixed-top">
                 <Link to="/" className="navbar-brand">AdPortal</Link>
                 <ul className="navbar-nav">
+                    {!isEditPage() && 
                     <li className="nav-item">
                         <Link to="/add" className="nav-link">Add offer</Link>
-                    </li>
+                    </li>}
                 </ul>
             </nav>
             
