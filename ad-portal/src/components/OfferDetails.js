aeef15c3ba6832d65fa904ec61c8835cbdba81c7
Filip Kaszuba <fkaszuba93@gmail.com>
2023-02-13 20:38:20 +0100

    show user info in offer details

--- ad-portal/src/components/OfferDetails.js
+++ ad-portal/src/components/OfferDetails.js
@@ -19,14 +19,26 @@ const OfferDetails = () => {
     }, [id]);
 
     return (
-        <div className="mx-5" style={{marginTop: "75px"}}>
+        <div className="mx-5" style={{marginTop: "5rem"}}>
             {offer.error ? <h2>{offer.error}</h2>
             : <>
-            <h1>{offer.title}</h1>
-            <div className="small">Category: {category}</div>
-            <div className="small">Created: {formatDate(offer.createDate)}</div>
-            <p className="lead py-4">{offer.description}</p>
-            </>}
+            <div className="container-fluid">
+                <h1>{offer.title}</h1>
+                <div className="small">Category: {category}</div>
+                <div className="small">Created: {formatDate(offer.createDate)}</div>
+                <div className="row">
+                    <div className="col-lg-9">
+                        <p className="lead py-4">{offer.description}</p>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="ml-3 my-4 p-4 border rounded bg-light">
+                            <div>Posted by: <span className="text-primary font-weight-bold">{offer.user?.username}</span></div>
+                            <div>email: <span className="font-weight-bold">{offer.user?.email}</span></div>
+                            {offer.user?.phone && <div>phone: <span className="font-weight-bold">{offer.user?.phone}</span></div>}
+                        </div>
+                    </div>
+                </div>
+            </div></>}
         </div>
     );
 };
